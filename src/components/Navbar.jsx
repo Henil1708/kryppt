18f5699874280436aa3f19ae140701eb15cd556d
HenilMehta <190510101071@paruluniversity.ac.in>
2022-06-14 20:58:10 +0530

    added portfolio and github link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
+import { BsArrowRightCircle, BsGithub } from "react-icons/bs";
 import logo from "../../images/logo.png";
 
 const NavbarItem = ({title, classProps}) => {
@@ -20,12 +21,10 @@ const Navbar = () => {
           <div className="md:flex-[0.5] flex-initial justify-center items-center ">
               <img src={logo} alt="logo" className="w-32 cursor-pointer " />
           </div>
-          <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial"> 
-              {["Market", "Exchange", "Tutorial", "Wallets"].map((item,index) => {
-                  return <NavbarItem title={item} key={item + index} />;
-              })}
-              <li className="bg-[#2952e3] py-2  px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
-                  Login
+          <ul className="text-white flex  list-none flex-row justify-between items-center flex-initial"> 
+              <li ><a href="https://github.com/Henil1708" target={"_blank"} rel="noopner noreferrer" className="md:px-5 px-3 rounded-md cursor-pointer md:mx-3 mx-1 hover:bg- bg-black/40 md:py-4 py-2 flex items-center justify-center backdrop-blur md:border-none border "><BsGithub fontSize={23} /> <span className="ml-2"> Github</span></a></li>
+              <li >
+                  <a href="https://henilmehta.me" target={"_blank"} rel="noopner noreferrer" className="bg-[#2952e3] flex text-sm items-center group justify-center py-2  px-7 md:mx-4 mx-2 rounded-full cursor-pointer hover:bg-[#2546bd]"><span className="mr-2">Portfolio</span><BsArrowRightCircle className="group-hover:ml-0 -ml-6  opacity-0 group-hover:opacity-100 duration-200" /></a>
               </li>
           </ul>
           <div className="flex relative ">
